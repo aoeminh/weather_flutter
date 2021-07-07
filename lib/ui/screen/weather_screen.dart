485b6585813fa8af22ead86f75ad4793c49c6ae4
aoeminh <aoeminh@gmail.com>
2021-07-07 17:12:17 +0700

    Revert "test 1"
    This reverts commit d979b1794573f623167b3da75ad232c0d776fabd.

--- lib/ui/screen/weather_screen.dart
+++ lib/ui/screen/weather_screen.dart
@@ -81,7 +81,6 @@ class _WeatherScreenState extends State<WeatherScreen>
   void initState() {
     super.initState();
     if (this.mounted) {
-      print('test 1');
       _listenListCityChange();
       _listenChangeSetting();
       appBloc.createInterstitialAd();
